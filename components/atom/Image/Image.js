83a817f7812eee3c15f996f90e9ea33c451227d3
sangjin149 <sangjin149@gmail.com>
2023-06-17 14:53:42 +0900

    debug: style unit error

--- components/atom/Image/Image.js
+++ components/atom/Image/Image.js
@@ -1,12 +1,11 @@
 import styled from "styled-components/native";
 
 export function Image({ width, height, src, onPress }) {
-    const setSize = (size) => {};
     return <ImageFrame width={width} height={height} src={src} onPress={onPress} />;
 }
 
 const ImageFrame = styled.Image`
-    width: ${({ width }) => width};
-    height: ${({ height }) => height};
-    border-radius: 0.5rem;
+    width: ${({ width }) => width}px;
+    height: ${({ height }) => height}px;
+    border-radius: 8px;
 `;
