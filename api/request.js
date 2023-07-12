df332bff28fa4644b1c8e3f640198caca3a3964c
sangjin149 <sangjin149@gmail.com>
2023-07-12 18:26:31 +0900

    chore: request에 JSON.stringify 추가

--- api/request.js
+++ api/request.js
@@ -2,11 +2,11 @@ import { API_END_POINT } from "../constants/api";
 
 export const request = async (url, options) => {
     const option = options;
-    option.headers = {
+    option.headers = JSON.stringify({
         ...option.headers,
         accept: "*/*",
         "Content-Type": "application/json",
-    };
+    });
 
     return fetch(`${API_END_POINT}/${url}`, option)
         .then((res) => {
