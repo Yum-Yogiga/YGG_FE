8a828b85720cbe0bfd613f86c4d3ad9c6876dce6
sangjin149 <sangjin149@gmail.com>
2023-07-14 14:04:11 +0900

    feature: FormInput style의 width와 margin 제거

--- components/molecule/FormInput/index.js
+++ components/molecule/FormInput/index.js
@@ -1,22 +1,16 @@
 import { React } from "react";
 import { StyleSheet, TextInput } from "react-native";
 
-export function FormInput({ error, ...props }) {
-    return <TextInput style={[styles.input, error ? styles.valueValid : styles.valueInvalid]} {...props} />;
+export function FormInput({ error, style, ...props }) {
+    return <TextInput style={[styles.input, error || styles.valueInvalid, style]} {...props} />;
 }
 
 const styles = StyleSheet.create({
     input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
         padding: 10,
     },
-    valueValid: {
-        borderColor: "black",
-    },
     valueInvalid: {
-        borderColor: "red",
+        color: "red",
     },
 });
 
