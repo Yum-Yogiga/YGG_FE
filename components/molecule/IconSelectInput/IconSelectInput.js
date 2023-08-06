0039ba15a2cba74517c781a6f1699f297c112d5e
sangjin149 <sangjin149@gmail.com>
2023-08-06 22:55:30 +0900

    feature: 현재 값에 따라 option 색상 변환 구현

--- components/molecule/IconSelectInput/IconSelectInput.js
+++ components/molecule/IconSelectInput/IconSelectInput.js
@@ -7,38 +7,38 @@ const EntrySets = {
     price: [
         {
             name: "가성비",
-            value: "cheap",
+            optionValue: "cheap",
         },
         {
             name: "여유롭게",
-            value: "moderate",
+            optionValue: "moderate",
         },
         {
             name: "FLEX",
-            value: "expensive",
+            optionValue: "expensive",
         },
     ],
     waiting: [
         {
             name: "거의 없음",
-            value: "no waiting",
+            optionValue: "no waiting",
         },
         {
             name: "약간 있음",
-            value: "moderate waiting",
+            optionValue: "moderate waiting",
         },
         {
             name: "사람 많음",
-            value: "long waiting",
+            optionValue: "long waiting",
         },
     ],
 };
 
 // 할 일
-// entry에 따라 보기 출력
+// entry에 따라 보기 출력 끝
 //   entrySet이 있다면 이 목록을 따르고, entry가 있다면 entry를 따름
-// iconSize 정하기
-// value에 따라 isSelected 지정해주기
+// iconSize 정하기 => 안 하는ㄱ 나음
+// value에 따라 isSelected 지정해주기 => 서옥ㅇ
 // 보기 선택시 onChanged 호출
 // error 전달시 출력
 
@@ -58,14 +58,15 @@ export const IconSelectInput = ({
         <Container>
             <Title>가격대</Title>
             <OptionList>
-                {entryInfo.map(({ name, value }) => {
+                {entryInfo.map(({ name, optionValue }) => {
                     return (
                         <Option
                             name={name}
                             size={iconSize}
                             onPress={() => {
-                                console.log(value);
+                                onChange(optionValue);
                             }}
+                            isSelected={value === optionValue}
                         />
                     );
                 })}
@@ -80,7 +81,7 @@ const Container = styled.View`
 `;
 const Title = styled.Text`
     padding: 8px 0px;
-    font-size: 32px;
+    font-size: 28px;
     text-align: center;
 `;
 const OptionList = styled.View`
