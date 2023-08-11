376344d87c61e501ee26df0ed0caaec2e42bae29
sangjin149 <sangjin149@gmail.com>
2023-08-11 17:11:01 +0900

    feature: 버튼을 통한 Input값 증감 구현

--- components/molecule/DistanceInput.js
+++ components/molecule/DistanceInput.js
@@ -1,17 +1,37 @@
-import { styled } from "styled-components/native";
+import { useState } from "react";
 import { Entypo } from "@expo/vector-icons";
+import { styled } from "styled-components/native";
 
 export const DistanceInput = () => {
+    const [value, setValue] = useState(0.5);
+    const step = 0.5;
+    const minValue = 0.5;
+    const maxValue = 2.0;
+    const [isMin, setIsMin] = useState(value - step <= minValue);
+    const [isMax, setIsMax] = useState(value + step >= maxValue);
+
+    const handlePressDecrease = () => {
+        if (value - step >= minValue) setValue(value - step);
+        if (value - step < minValue) setIsMin(true);
+        if (value + step < maxValue) setIsMax(false);
+    };
+
+    const handlePressIncrease = () => {
+        if (value + step <= maxValue) setValue(value + step);
+        if (value + step > maxValue) setIsMax(true);
+        if (value - step > minValue) setIsMin(false);
+    };
+
     return (
         <Container>
-            <ControlButton>
-                <Entypo name="plus" size={28} color="white" />
+            <ControlButton disabled={isMin} onPress={handlePressDecrease}>
+                <Entypo name="minus" size={28} color="white" />
             </ControlButton>
             <ValueDisplay>
-                <ValueText>1</ValueText>
+                <ValueText>{value}km</ValueText>
             </ValueDisplay>
-            <ControlButton>
-                <Entypo name="minus" size={28} color="white" />
+            <ControlButton disabled={isMax} onPress={handlePressIncrease}>
+                <Entypo name="plus" size={28} color="white" />
             </ControlButton>
         </Container>
     );
@@ -29,10 +49,11 @@ const ControlButton = styled.TouchableOpacity`
     border-radius: 28px;
     justify-content: center;
     align-items: center;
-    background-color: #ffaf42;
+    background-color: ${({ disabled }) => (disabled ? "#a9a9a9" : "#ffaf42")};
 `;
 
 const ValueDisplay = styled.View`
+    flex-direction: column;
     width: 200px;
     height: 56px;
     justify-content: center;
