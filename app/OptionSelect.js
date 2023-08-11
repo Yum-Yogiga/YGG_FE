c84b8e7168935b4a176141fff339af5dc62118c1
sangjin149 <sangjin149@gmail.com>
2023-08-11 20:05:23 +0900

    feature: OptionSelect 페이지 레이아웃 작성

--- app/OptionSelect.js
+++ app/OptionSelect.js
@@ -1,5 +1,34 @@
-import { Text } from "react-native";
+import { View, Text } from "react-native";
+import { DistanceInput } from "../components/molecule/DistanceInput";
+import { styled } from "styled-components/native";
+import { IconSelectInput } from "../components/molecule/IconSelectInput/IconSelectInput";
 
 export default function OptionSelect() {
-    return <Text>OptionSelectPage</Text>;
+    return (
+        <Container>
+            <>
+                <View style={{ marginTop: 72 }}>
+                    <Label>거리</Label>
+                    <DistanceInput />
+                </View>
+                <View style={{ marginTop: 50 }}>
+                    <Label>가격</Label>
+                    <IconSelectInput entrySetName="price" iconSize={56} onChange={() => true} error={""} />
+                </View>
+                <View style={{ marginTop: 26 }}>
+                    <Label>웨이팅</Label>
+                    <IconSelectInput entrySetName="waiting" iconSize={56} onChange={() => true} error={""} />
+                </View>
+            </>
+        </Container>
+    );
 }
+
+const Container = styled.View`
+    flex: 1;
+`;
+
+const Label = styled.Text`
+    font-size: 24px;
+    text-align: center;
+`;
