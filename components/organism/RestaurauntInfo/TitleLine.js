e7fd9546e0c21c05660814e2a0b761602529865d
sangjin149 <sangjin149@gmail.com>
2023-10-29 22:58:05 +0900

    featuer: TitleLine 레이아웃 구현

---
+++ components/organism/RestaurauntInfo/TitleLine.js
@@ -0,0 +1,44 @@
+import { styled } from "styled-components/native";
+
+export function TitleLine() {
+    return (
+        <Container>
+            <TitleContainer>
+                <Title numberOfLines={1}>투파인드피터 가나다라마ㅏ만 홍대점</Title>
+                <Subtitle numberOfLines={1}>양식</Subtitle>
+            </TitleContainer>
+            <Dummy />
+        </Container>
+    );
+}
+
+const Container = styled.View`
+    width: 100%;
+    height: 72px;
+    flex-direction: row;
+    border: 1px solid black;
+`;
+
+const TitleContainer = styled.View`
+    width: 75%;
+    height: 100%;
+    flex-direction: column;
+`;
+
+const Title = styled.Text`
+    padding: 2px;
+    font-size: 18px;
+    align-items: center;
+`;
+
+const Subtitle = styled.Text`
+    padding: 2px;
+    font-size: 16px;
+    align-items: center;
+`;
+
+const Dummy = styled.View`
+    width: 25%;
+    height: 100%;
+    background-color: black;
+`;
--- components/organism/index.js
+++ components/organism/index.js
@@ -1,1 +1,2 @@
 export { LoginForm } from "./LoginForm/LoginForm.js";
+export { TitleLine } from "./RestaurauntInfo/TitleLine.js";
