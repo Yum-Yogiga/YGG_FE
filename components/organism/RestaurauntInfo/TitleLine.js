5762152df79ce1b413eba105dc6162c1f7fdcb4b
sangjin149 <sangjin149@gmail.com>
2023-10-30 01:03:10 +0900

    feature: TitleLine 컴포넌트 매개변수 입력

--- components/organism/RestaurauntInfo/TitleLine.js
+++ components/organism/RestaurauntInfo/TitleLine.js
@@ -1,22 +1,27 @@
 import { styled } from "styled-components/native";
 import { RecommendRate } from "molecule/RecommendRate";
 
-export function TitleLine() {
+export function TitleLine({
+    title = "식당 상호나 이름이 들어가는 자리",
+    subtitle = "식당 유형이 들어가는 자리",
+    rate = 25,
+}) {
     return (
         <Container>
             <TitleContainer>
-                <Title numberOfLines={1}>투파인드피터 가나다라마ㅏ만 홍대점</Title>
-                <Subtitle numberOfLines={1}>양식</Subtitle>
+                <Title numberOfLines={1}>{title}</Title>
+                <Subtitle numberOfLines={1}>{subtitle}</Subtitle>
             </TitleContainer>
-            <RecommendRate rate={75} />
+            <RecommendRate rate={rate} />
         </Container>
     );
 }
 
 const Container = styled.View`
+    padding: 8px 0px;
     width: 100%;
-    height: 72px;
     flex-direction: row;
+    border: 1px solid black;
 `;
 
 const TitleContainer = styled.View`
