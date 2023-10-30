60327c94dbdc7432e56d61508053b48ce33118cb
sangjin149 <sangjin149@gmail.com>
2023-10-30 09:43:40 +0900

    feature: address 매개변수명 변경

--- components/organism/RestaurauntInfo/Address.js
+++ components/organism/RestaurauntInfo/Address.js
@@ -1,13 +1,13 @@
 import { styled } from "styled-components/native";
 import { Octicons } from "@expo/vector-icons";
 
-export function Address({ content = "대충 주소 들어가는 자리" }) {
+export function Address({ address = "대충 주소 들어가는 자리" }) {
     return (
         <Container>
             <Icon>
                 <Octicons name="location" size={20} color="#9D9D9D" />
             </Icon>
-            <Content numberOfLines={2}>{content}</Content>
+            <Content numberOfLines={2}>{address}</Content>
         </Container>
     );
 }
