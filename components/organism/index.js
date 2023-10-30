b7ef7332422b35910881e9cc34b107d761a05973
sangjin149 <sangjin149@gmail.com>
2023-10-30 09:44:02 +0900

    feature: MenuList 매개변수명 입력

--- components/organism/index.js
+++ components/organism/index.js
@@ -2,3 +2,4 @@ export { LoginForm } from "./LoginForm/LoginForm.js";
 export { TitleLine } from "./RestaurauntInfo/TitleLine.js";
 export { Address } from "./RestaurauntInfo/Address.js";
 export { Distance } from "./RestaurauntInfo/Distance.js";
+export { MenuList } from "./RestaurauntInfo/MenuList.js";
