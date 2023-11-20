05882a0267996241c7cad79f88982ef8ea73a33b
sangjin149 <sangjin149@gmail.com>
2023-11-20 22:41:15 +0900

    어플리케이션 입장시 자동으로 로그인 화면 리다이렉트

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 import { SafeAreaView, StyleSheet } from "react-native";
+import { Redirect } from "expo-router";
 
 import { Link } from "expo-router";
 
 export default function App() {
+    return <Redirect href="/auth/login" />;
+
     return (
         <SafeAreaView style={styles.container}>
             <Link href="/auth/login">로그인 화면</Link>
