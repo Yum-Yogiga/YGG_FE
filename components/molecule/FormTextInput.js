90b200aa735f069110dcd7d38fe3f4ec8f81f4f9
sangjin149 <sangjin149@gmail.com>
2023-11-22 15:50:12 +0900

    이메일 입력시 이메일 자판으로 전환

--- components/molecule/FormTextInput.js
+++ components/molecule/FormTextInput.js
@@ -3,8 +3,16 @@ import { StyleSheet, TextInput } from "react-native";
 
 export function FormTextInput({ type, error = false, style, ...props }) {
     const secureText = type === "password" || type === "password_verification";
+    const inputMode = type;
 
-    return <TextInput style={[error && styles.valueInvalid, style]} secureTextEntry={secureText} {...props} />;
+    return (
+        <TextInput
+            style={[error && styles.valueInvalid, style]}
+            secureTextEntry={secureText}
+            inputMode={type === "email" ? "email" : "none"}
+            {...props}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
