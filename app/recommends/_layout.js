21dc246d720a3c0a2837412d80cd2897ba083130
sangjin149 <sangjin149@gmail.com>
2023-11-23 03:01:56 +0900

    restId Context 장착 및 restinfo 페이지 restid로 개명

--- app/recommends/_layout.js
+++ app/recommends/_layout.js
@@ -2,32 +2,35 @@ import { useRouter } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Stack } from "expo-router/stack";
 import { AntDesign } from "@expo/vector-icons";
+import { RestIdProvider } from "../context/RestaurantIdContext";
 
 export default function Layout() {
     const router = useRouter();
 
     return (
         <SafeAreaProvider>
-            <Stack
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: "white",
-                    },
-                    headerLeft: () => (
-                        <AntDesign
-                            name="left"
-                            size={24}
-                            color="black"
-                            onPress={() => {
-                                router.push("/setkeywords/keywordselect");
-                            }}
-                        />
-                    ),
-                    title: null,
-                }}
-            >
-                <Stack.Screen name="waitingRecommend" options={{ headerShown: false }} />
-            </Stack>
+            <RestIdProvider>
+                <Stack
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: "white",
+                        },
+                        headerLeft: () => (
+                            <AntDesign
+                                name="left"
+                                size={24}
+                                color="black"
+                                onPress={() => {
+                                    router.push("/setkeywords/keywordselect");
+                                }}
+                            />
+                        ),
+                        title: null,
+                    }}
+                >
+                    <Stack.Screen name="waitingRecommend" options={{ headerShown: false }} />
+                </Stack>
+            </RestIdProvider>
         </SafeAreaProvider>
     );
 }
