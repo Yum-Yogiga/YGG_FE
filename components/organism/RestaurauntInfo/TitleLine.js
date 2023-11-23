e12160d3c4a19984157d213e364020efa7900aed
sangjin149 <sangjin149@gmail.com>
2023-11-23 10:40:27 +0900

    타이틀라인 레이아웃 수정

--- components/organism/RestaurauntInfo/TitleLine.js
+++ components/organism/RestaurauntInfo/TitleLine.js
@@ -2,42 +2,39 @@ import { styled } from "styled-components/native";
 import { RecommendRate } from "molecule/RecommendRate";
 
 export function TitleLine({
-    title = "식당 상호나 이름이 들어가는 자리",
-    subtitle = "식당 유형이 들어가는 자리",
-    rate = 25,
+  title = "식당 상호나 이름이 들어가는 자리",
+  rate = 25,
 }) {
-    return (
-        <Container>
-            <TitleContainer>
-                <Title numberOfLines={1}>{title}</Title>
-                <Subtitle numberOfLines={1}>{subtitle}</Subtitle>
-            </TitleContainer>
-            <RecommendRate rate={rate} />
-        </Container>
-    );
+  return (
+    <Container>
+      <TitleContainer>
+        <Title numberOfLines={1}>{title}</Title>
+      </TitleContainer>
+      <RecommendRate rate={rate} />
+    </Container>
+  );
 }
 
 const Container = styled.View`
-    height: 72px;
-    width: 100%;
-    flex-direction: row;
-    align-items: center;
+  height: 72px;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const TitleContainer = styled.View`
-    height: 100%;
-    flex: 1;
-    flex-direction: column;
+  height: 100%;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const Title = styled.Text`
-    padding: 2px;
-    font-size: 18px;
-    align-items: center;
+  font-size: 18px;
 `;
 
 const Subtitle = styled.Text`
-    padding: 2px;
-    font-size: 16px;
-    align-items: center;
+  padding: 2px;
+  font-size: 16px;
+  align-items: center;
 `;
